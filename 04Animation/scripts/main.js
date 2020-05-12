943ec95a7aefe008226af457af9ec8626c71c167
Adam <adam.cerven@gmail.com>
2020-05-12 15:26:24 +0200

    change

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -2,8 +2,12 @@
 
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.01;
+var rotationTorus = 0.02
 var positive = true;
 var myOtherBox = document.getElementById('myOtherBox');
+var torus = document.getElementById('torus');
+var myBox = document.getElementById('myBox');
+var move = 0.1;
 
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
@@ -11,7 +15,7 @@ function spin(){
   myOtherBox.object3D.rotation.z -= rotationSpeed;
   if(positive){
     rotationSpeed += 0.001;
-    if(rotationSpeed > 0.1){
+    if(rotationSpeed > 0.05){
       positive = false;
     }
   }
@@ -21,7 +25,24 @@ function spin(){
       positive = true;
     }
   }
-	console.log(myOtherBox.object3D.rotation.x);
+}
+
+function spinTorus(){
+  torus.object3D.rotation.y += rotationTorus;
+}
+
+function moveBoxes(){
+  myOtherBox.object3D.position.x += move;
+  if(myOtherBox.object3D.position.x > 10 || myOtherBox.object3D.position.x < -10 ){
+    move = -(move);
+    console.log("executed");
+  }
+  console.log(myOtherBox.object3D.position.x);
+
+
+  //myOtherBox.object3D.position.z += rotationSpeed;
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
+setInterval(spinTorus, 32);
+setInterval(moveBoxes, 16);
