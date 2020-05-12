3c50156637e4bddc07667add045ce68a8a6b841d
Adam <adam.cerven@gmail.com>
2020-05-12 15:00:48 +0200

    apfij

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -2,10 +2,25 @@
 
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.01;
+var positive = true;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
+	myOtherBox.object3D.rotation.x -= rotationSpeed;
+  myOtherBox.object3D.rotation.y -= rotationSpeed;
+  myOtherBox.object3D.rotation.z -= rotationSpeed;
+  if(positive){
+    rotationSpeed += 0.001;
+    if(rotationSpeed > 0.1){
+      positive = false;
+    }
+  }
+  if(!positive){
+    rotationSpeed -= 0.001;
+    if(rotationSpeed <= 0.01){
+      positive = true;
+    }
+  }
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
